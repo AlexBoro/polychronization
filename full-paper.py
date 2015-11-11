0a8f701d3faded785e0298b9164f91d7fbdc8589
Garibaldi Pineda Garcia <chanokin@gmail.com>
2015-11-11 15:10:16 +0000

    changes to full paper

--- full-paper.py
+++ full-paper.py
@@ -33,9 +33,9 @@ def split_times_pops(full_times, pop1_size, pop2_size):
 
 #millisecond*second*minutes*hour in a day
 #runtime = 1000*60*60*24 
-runtime = 1000
-
-weight_to_spike = 0.1
+runtime  = 5000
+stimtime = 100
+weight_to_spike = 6.
 
 timestep = 1.
 min_delay = 1.
@@ -47,8 +47,9 @@ sim.setup(timestep=timestep, min_delay = min_delay, max_delay = max_delay)
 max_weight = 10.
 min_weight = 0.0
 a_plus = 0.1
-a_minus = 0.12
-tau = 20.
+a_minus = 0.3# 0.12
+tau_plus = 20.
+tau_minus = 30.
 
 num_exc = 800
 num_inh = 200
@@ -76,7 +77,7 @@ inh_params = {'a': 0.1,
              }
 init_inh_weight = 5.
 
-total_stim = random_thalamic_input((runtime*1)/4, total_neurons)
+total_stim = random_thalamic_input(stimtime, total_neurons)
 exc_stim_times, inh_stim_times = split_times_pops(total_stim, num_exc, num_inh)
 
 
@@ -96,7 +97,7 @@ stimI_pop = sim.Population(num_inh, sim.SpikeSourceArray,
                           label="inh network stimulation")
 
 stdp_model = sim.STDPMechanism(
-    timing_dependence=sim.SpikePairRule(tau_plus=tau, tau_minus=tau,
+    timing_dependence=sim.SpikePairRule(tau_plus=tau_plus, tau_minus=tau_minus,
                                         nearest=True),
     weight_dependence=sim.MultiplicativeWeightDependence(w_min=min_weight, w_max=max_weight,
                                                    A_plus=a_plus, A_minus=a_minus)
@@ -142,7 +143,7 @@ e2i_proj = sim.Projection(exc_pop, inh_pop, conn_exc, target="excitatory",
 #~ print("Inhibitory to Inhibitory connections")
 #~ print("-----------------------------------------------------------------")
 i2i_proj = sim.Projection(inh_pop, inh_pop, conn_inh, target="inhibitory",
-                           synapse_dynamics = sim.SynapseDynamics(slow = stdp_model))
+                          synapse_dynamics = sim.SynapseDynamics(slow = stdp_model))
 
 #~ print("-----------------------------------------------------------------")
 #~ print("-----------------------------------------------------------------")
@@ -182,6 +183,12 @@ s2i_proj = sim.Projection(stimI_pop, inh_pop, o2o_conn, target="excitatory")
 #~ print("-----------------------------------------------------------------")
 #~ print(i2e_proj.getWeights())
 
+print("-----------------------------------------------------------------")
+print("-----------------------------------------------------------------")
+print("Stim to Inh Weights")
+print("-----------------------------------------------------------------")
+print(s2i_proj.getWeights())
+
 exc_pop.record()
 inh_pop.record()
 
@@ -203,41 +210,57 @@ except IndexError:
 
 
 #pylab.ion()
+spike_times = []
+spike_ids = []
+time_window = 1000
 
 if exc_spikes_found or inh_spikes_found:
-  time_window = 1000
+  
   for start_time in xrange(0, runtime, time_window): #plot 1 sec at a time
     fig = pylab.figure()
+    
     #~ ax = fig.gca()
     #~ ax.set_xticks(numpy.arange(0,  runtime + 1, 5))
     #~ ax.set_yticks(numpy.arange(-1, num_neurons + 1, 1.) )
     #pylab.xlim([0,runtime+1])
-    pylab.ylim([-0.2,total_neurons+0.2])
+    #pylab.ylim([-0.2,total_neurons+0.2])
     end_time = start_time + time_window
-    
-    neuron_id = 0
-    for neuron_spikes in total_stim:
-      for spike_time in neuron_spikes:
-        pylab.plot(spike_time, neuron_id, "o", markerfacecolor="None",
-                   markeredgecolor="Green", markersize=4)
-      neuron_id += 1
-    
+    fig.suptitle("From %s ms to %s ms", start_time, end_time)
     if exc_spikes_found:
-      exc_spike_times = [spike_time for (neuron_id, spike_time) in exc_spikes \
+      spike_times[:] = []
+      spike_ids[:] = []
+
+      spike_times = [spike_time for (neuron_id, spike_time) in exc_spikes \
                          if start_time <= spike_time < end_time]
-      exc_spike_ids   = [neuron_id  for (neuron_id, spike_time) in exc_spikes \
+      spike_ids   = [neuron_id  for (neuron_id, spike_time) in exc_spikes \
                          if start_time <= spike_time < end_time]
-      pylab.plot(exc_spike_times, exc_spike_ids, ".", markerfacecolor="None",
+      pylab.plot(spike_times, spike_ids, ".", markerfacecolor="None",
                  markeredgecolor="Blue", markersize=1)
 
     if inh_spikes_found:
-      inh_spike_times = [spike_time for (neuron_id, spike_time) in inh_spikes \
+      spike_times[:] = []
+      spike_ids[:] = []
+
+      spike_times = [spike_time for (neuron_id, spike_time) in inh_spikes \
                          if start_time <= spike_time < end_time]
-      inh_spike_ids   = [neuron_id + num_exc for (neuron_id, spike_time) in inh_spikes \
+      spike_ids   = [neuron_id + num_exc for (neuron_id, spike_time) in inh_spikes \
                          if start_time <= spike_time < end_time]
-      pylab.plot(inh_spike_times, inh_spike_ids, ".", markerfacecolor="None",
+      pylab.plot(spike_times, spike_ids, ".", markerfacecolor="None",
                  markeredgecolor="Red", markersize=1)
 
+    if len(total_stim) > start_time:
+      spike_times[:] = []
+      spike_ids[:] = []
+      neuron_id = 0
+      for neuron_spikes in total_stim:
+        for spike_time in neuron_spikes:
+          spike_times.append(spike_time)
+          spike_ids.append(neuron_id)
+
+        neuron_id += 1
+      pylab.plot(spike_times, spike_ids, "o", markerfacecolor="None",
+                 markeredgecolor="Green", markersize=4)
+      
     dirname = "results"
     if not(os.path.isdir(dirname)):
       os.mkdir(dirname)
@@ -245,34 +268,34 @@ if exc_spikes_found or inh_spikes_found:
     fig_file = open(os.path.join(dirname,filename), 'w')
     pylab.savefig(fig_file)
 
-    pylab.show()
+  pylab.show()
 
 # delta < 4 Hz == 250 ms or more
 # alpha 8 to 12 Hz == 125 to 83 ms
 # gamma > 32 Hz == 32 ms or less
 
-print("-----------------------------------------------------------------")
+print("\n\n\n\n\n\n-----------------------------------------------------------------")
 print("-----------------------------------------------------------------")
 print("Exc to Exc Weights")
 print("-----------------------------------------------------------------")
 print(e2e_proj.getWeights())
 
 
-print("-----------------------------------------------------------------")
+print("\n\n\n\n\n\n-----------------------------------------------------------------")
 print("-----------------------------------------------------------------")
 print("Exc to Inh Weights")
 print("-----------------------------------------------------------------")
 print(e2i_proj.getWeights())
 
 
-print("-----------------------------------------------------------------")
+print("\n\n\n\n\n\n-----------------------------------------------------------------")
 print("-----------------------------------------------------------------")
 print("Inh to Inh Weights")
 print("-----------------------------------------------------------------")
 print(i2i_proj.getWeights())
 
 
-print("-----------------------------------------------------------------")
+print("\n\n\n\n\n\n-----------------------------------------------------------------")
 print("-----------------------------------------------------------------")
 print("Inh to Exc Weights")
 print("-----------------------------------------------------------------")
